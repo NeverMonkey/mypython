5745c101571d5bf7130374035fbb87c07041c73f
mi-vm-mycentos <usethepython@126.com>
2018-06-06 16:48:12 +0800

    split lines

--- check_passwd.py
+++ check_passwd.py
@@ -8,7 +8,9 @@
 
 def checkio(data):
     if len(data) > 9:
-        if any(i.isupper() for i in data) and any(i.islower() for i in data) and any(i.isdigit() for i in data):
+        if any(i.isupper() for i in data) \
+        and any(i.islower() for i in data) \
+        and any(i.isdigit() for i in data):
             return True
         else:
             return False
