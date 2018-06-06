9860c3d1b3d823345d9664f8c35b103b8599d775
mi-vm-mycentos <usethepython@126.com>
2018-06-06 17:05:58 +0800

    duplicate 2nd example

--- check_passwd.py
+++ check_passwd.py
@@ -29,6 +29,7 @@ if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
     assert checkio('A1213pokl') == False, "1st example"
     assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('bAse730onE4') == True, "2nd example"
     assert checkio('asasasasasasasaas') == False, "3rd example"
     assert checkio('QWERTYqwerty') == False, "4th example"
     assert checkio('123456123456') == False, "5th example"
